9ce10fa98a90b0a88fc39e233e382926ae42f169
Hero Ahmadi <hero.lionel@gmail.com>
2018-11-30 11:20:46 +0700

    vagrant webserver

--- zatt/provision.sh
+++ zatt/provision.sh
@@ -1,3 +1,4 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-setuptools python3-dev
 sudo python3 /vagrant/setup.py install
+sudo python3 /vagrant/setup.py install
---
+++ zatt/provision_webserver.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y python3-pip
+export LC_ALL=C
+sudo pip3 install --upgrade pip
+sudo pip3 install flask
